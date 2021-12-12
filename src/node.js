ab2b5b013e4012990bf318dff00655ae17dc0733
Yair <32864499+bluepostit@users.noreply.github.com>
2021-12-12 16:09:38 +0100

    create children nodes from data

--- src/node.js
+++ src/node.js
@@ -1,17 +1,27 @@
 class Node {
-  constructor(data, parent) {
+  constructor(data, parent = null) {
     this._parent = parent
     this._data = data
+    this.children = []
 
     Object.getOwnPropertyNames(data).forEach(key => {
       if (key === 'parent' || key === '_parent') {
         return
+      } else if (key === 'children') {
+        data[key].forEach(({ data }) => {
+          const childNode = new Node(data, this)
+          this.children.push(childNode)
+        })
+        return
       }
 
       Object.defineProperty(this, key, {
-        get: () => { return data[key] }
+        get: () => {
+          return data[key]
+        }
       })
     })
+    console.log(this.children)
   }
 
   get parent() {
--- test/node.test.js
+++ test/node.test.js
@@ -13,14 +13,14 @@ describe('Node', () => {
 
     it('should create a Node with a getter for each data key', () => {
       const data = {
-        children: [],
+        unicorns: [],
         path: 'test-path'
       }
       const parent = {
         title: 'test title'
       }
       const node = new Node(data, parent)
-      expect(node.children).toBe(data.children)
+      expect(node.unicorns).toBe(data.unicorns)
       expect(node.path).toBe(data.path)
     })
 
@@ -34,6 +34,54 @@ describe('Node', () => {
       }
       const node = new Node(data, parent)
       expect(node.parent).toBe(parent)
+    })
+
+    it('should create a node with empty parent when given no parent', () => {
+      const data = {
+        unicorns: [],
+        path: 'test-path'
+      }
+
+      const node = new Node(data)
+      expect(node.parent).toBeNull()
+    })
+
+    it('should create Node elements of given children in data', () => {
+      const data = {
+        children: [
+          {
+            data: {
+              name: 'child 1'
+            }
+          },
+          {
+            data: {
+              name: 'child 2'
+            }
+          }
+        ]
+      }
+      const node = new Node(data)
+      const children = node.children
+      expect(children[0]).toBeInstanceOf(Node)
+      expect(children[0].name).toBe(children[0].data.name)
+    })
+
+    it('should ignore parent data of given children', () => {
+      const data = {
+        children: [
+          {
+            data: {
+              name: 'child 1',
+              parent: 'fake parent'
+            }
+          }
+        ]
+      }
+      const node = new Node(data)
+      const children = node.children
+      expect(children[0]).toBeInstanceOf(Node)
+      expect(children[0].parent).toBe(node)
     })
   })
 })
