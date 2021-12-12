def47b71cf46506c079078f70487eaa65988e436
Yair <32864499+bluepostit@users.noreply.github.com>
2021-12-12 15:52:48 +0100

    create getters based on data

--- src/node.js
+++ src/node.js
@@ -2,6 +2,16 @@ class Node {
   constructor(data, parent) {
     this._parent = parent
     this._data = data
+
+    Object.getOwnPropertyNames(data).forEach(key => {
+      if (key === 'parent' || key === '_parent') {
+        return
+      }
+
+      Object.defineProperty(this, key, {
+        get: () => { return data[key] }
+      })
+    })
   }
 
   get parent() {
--- test/node.test.js
+++ test/node.test.js
@@ -11,15 +11,29 @@ describe('Node', () => {
       expect(node.parent).toBe(parent)
     })
 
-    it('should create a Node with the given data', () => {
+    it('should create a Node with a getter for each data key', () => {
       const data = {
-        children: []
+        children: [],
+        path: 'test-path'
       }
       const parent = {
         title: 'test title'
       }
       const node = new Node(data, parent)
-      expect(node.data).toBe(data)
+      expect(node.children).toBe(data.children)
+      expect(node.path).toBe(data.path)
+    })
+
+    it('should not create a getter for parent from data', () => {
+      const data = {
+        parent: 'test-parent',
+        _parent: 'test-parent'
+      }
+      const parent = {
+        title: 'test title'
+      }
+      const node = new Node(data, parent)
+      expect(node.parent).toBe(parent)
     })
   })
 })
